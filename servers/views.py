0315d23729d0a27f6e20b6a0f917e8ecde135da2
Yvette-Lau <lauyatting9598@gmail.com>
2020-03-31 08:36:26 -0300

    for-block

--- servers/views.py
+++ servers/views.py
@@ -7,75 +7,75 @@ import json
 
 # Create your views here.
 
-def serverInfo(request):
-    data = {
-        'servers': [
-            {
-                'name': 'rhlab1',
-                'os': 'Linux',
-                'logicalDrive': '256GB',
-                'processors': '4 Cores',
-                'ram': 'TBC',
-                'location': 'Halifax',
-                'ipInternal': '10.100.107.23',
-                'subnetMask': '255.155.255.0',
-                'gateway': '10.100.107.254',
-                'dns': 'TBC',
-                'vlan': 'TBC',
-                'ipExternal': 'TBC',
-                'mns': 'TBC',
-            },
-            {
-                'name': 'rhlab1',
-                'os': 'Linux',
-                'logicalDrive': '256GB',
-                'processors': '4 Cores',
-                'ram': 'TBC',
-                'location': 'Halifax',
-                'ipInternal': '10.100.107.23',
-                'subnetMask': '255.155.255.0',
-                'gateway': '10.100.107.254',
-                'dns': 'TBC',
-                'vlan': 'TBC',
-                'ipExternal': 'TBC',
-                'mns': 'TBC',
-            },
-            {
-                'name': 'rhlab1',
-                'os': 'Linux',
-                'logicalDrive': '256GB',
-                'processors': '4 Cores',
-                'ram': 'TBC',
-                'location': 'Halifax',
-                'ipInternal': '10.100.107.23',
-                'subnetMask': '255.155.255.0',
-                'gateway': '10.100.107.254',
-                'dns': 'TBC',
-                'vlan': 'TBC',
-                'ipExternal': 'TBC',
-                'mns': 'TBC',
-            },
-        ],
-    }
-    return JsonResponse(data)
+# def serverInfo(request):
+#     data = {
+#         'servers': [
+#             {
+#                 'name': 'rhlab1',
+#                 'os': 'Linux',
+#                 'logicalDrive': '256GB',
+#                 'processors': '4 Cores',
+#                 'ram': 'TBC',
+#                 'location': 'Halifax',
+#                 'ipInternal': '10.100.107.23',
+#                 'subnetMask': '255.155.255.0',
+#                 'gateway': '10.100.107.254',
+#                 'dns': 'TBC',
+#                 'vlan': 'TBC',
+#                 'ipExternal': 'TBC',
+#                 'mns': 'TBC',
+#             },
+#             {
+#                 'name': 'rhlab1',
+#                 'os': 'Linux',
+#                 'logicalDrive': '256GB',
+#                 'processors': '4 Cores',
+#                 'ram': 'TBC',
+#                 'location': 'Halifax',
+#                 'ipInternal': '10.100.107.23',
+#                 'subnetMask': '255.155.255.0',
+#                 'gateway': '10.100.107.254',
+#                 'dns': 'TBC',
+#                 'vlan': 'TBC',
+#                 'ipExternal': 'TBC',
+#                 'mns': 'TBC',
+#             },
+#             {
+#                 'name': 'rhlab1',
+#                 'os': 'Linux',
+#                 'logicalDrive': '256GB',
+#                 'processors': '4 Cores',
+#                 'ram': 'TBC',
+#                 'location': 'Halifax',
+#                 'ipInternal': '10.100.107.23',
+#                 'subnetMask': '255.155.255.0',
+#                 'gateway': '10.100.107.254',
+#                 'dns': 'TBC',
+#                 'vlan': 'TBC',
+#                 'ipExternal': 'TBC',
+#                 'mns': 'TBC',
+#             },
+#         ],
+#     }
+#     return JsonResponse(data)
 
 
 def index(request):
     return render(request, 'index.html')
 
 
-# def serverInfo(request):
-#     try:
-#         serverName = request.GET.get("serverName")
-#         if serverName == None:
-#             servers = ServerInfo.object.all().values()
-#             return JsonResponse({'servers': list(servers)})
-#         else:
-#             servers = ServerInfo.object.filter(serverName=serverName).values()
-#             return JsonResponse({'servers': list(servers)})
-#
-#         # server_json = [{'servers':list(servers)}]
-#         # print(server_json)
-#         # return HttpResponse(json.dump(server_json), 'application/json')
-#     except:
-#         return "This server not exist! "
+def serverInfo(request):
+    try:
+        serverName = request.GET.get("serverName")
+        if serverName == None:
+            servers = ServerInfo.object.all().values()
+            return JsonResponse({'servers': list(servers)})
+        else:
+            servers = ServerInfo.object.filter(serverName=serverName).values()
+            return JsonResponse({'servers': list(servers)})
+
+        # server_json = [{'servers':list(servers)}]
+        # print(server_json)
+        # return HttpResponse(json.dump(server_json), 'application/json')
+    except:
+        return "This server not exist! "
